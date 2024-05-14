b443d48413f0d46c09cd7bd7ca8f4387a9b5023e
Shivam Rathi <shivamrathi011@gmail.com>
2024-05-14 20:46:33 +0530

    14-5-2024

---
+++ Jenkins/roles/jenkins/files/updateKey.sh
@@ -0,0 +1,5 @@
+wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
